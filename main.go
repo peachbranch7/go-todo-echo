5656d50b7fb105df96edd0fda0ea10fa871f642c
peachbranch7 <peachbranch.0517@gmail.com>
2024-06-19 17:00:47 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+	"go-rest-api/validator"
+)
+
+func main() {
+	db := db.NewDb()
+	userValidator := validator.NewUserValidator()
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	userController := controller.NewUserController(userUsecase)
+	
+	taskValidator := validator.NewTaskValidator()
+	taskRepository := repository.NewTaskRepository(db)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
+	taskController := controller.NewTaskController(taskUsecase)
+
+	e := router.NewRouter(userController, taskController)
+	e.Logger.Fatal(e.Start(":8080"))
+}
